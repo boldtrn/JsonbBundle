56c822f5e976cc0f85d03c4afbb6e10fd26d6c13
Robin <boldtrn@gmail.com>
2015-10-22 20:55:27 +0200

    Added empty array Test

--- Tests/IntegrationTest/IntegrationTest.php
+++ Tests/IntegrationTest/IntegrationTest.php
@@ -18,16 +18,50 @@ class IntegrationTest extends BaseTest
     public function testSimpleInsertSelect()
     {
 
-        $test = new Test();
-        $test->setAttrs(array('foo' => 'bar'));
-
-        $this->entityManager->persist($test);
-        $this->entityManager->flush();
+        $test = $this->createTest(array('foo' => 'bar'));
 
         /** @var Test $retrievedTest */
         $retrievedTest = $this->entityManager->getRepository($this->testEntityName)->find($test->getId());
 
         static::assertEquals($test->getAttrs(), $retrievedTest->getAttrs());
+
+    }
+
+    public function testEmptyArray()
+    {
+
+        $test = $this->createTest(array());
+
+        /** @var Test $retrievedTest */
+        $retrievedTest = $this->entityManager->getRepository($this->testEntityName)->find($test->getId());
+
+        static::assertEquals($test->getAttrs(), $retrievedTest->getAttrs());
+
+    }
+
+    /**
+     * @param $attrs array the attributes of the jsonb array
+     * @return Test
+     */
+    private function createTest($attrs)
+    {
+        $test = new Test();
+        $test->setAttrs($attrs);
+
+        $this->entityManager->persist($test);
+        $this->entityManager->flush();
+
+        return $test;
+    }
+
+    private function clearTable()
+    {
+
+        foreach ($this->entityManager->getRepository($this->testEntityName)->findAll() as $test) {
+            $this->entityManager->remove($test);
+        }
+
+        $this->entityManager->flush();
 
     }
 
